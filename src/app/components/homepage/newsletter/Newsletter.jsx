b7db3a927e0637a6afa8b667ffcf79a561be9aea
John Kaburu <32298219+blackninja81@users.noreply.github.com>
2024-11-19 17:03:12 +0300

    [FEATURE] added news and newsletter sections

--- src/app/components/homepage/newsletter/Newsletter.jsx
+++ src/app/components/homepage/newsletter/Newsletter.jsx
@@ -1,9 +1,31 @@
 import React from "react";
 import "./newsletter.scss";
+
 const Newsletter = () => {
   return (
-    <div className="news-container">
-      <h2>Subscribe to out newsletter</h2>
+    <div className="news-container"
+    style={{ backgroundImage: `url(/assets/images/peru.webp)` }}
+    >
+    <div className="subscribe">
+      <h2 className="subscribe__title">Let's keep in touch</h2>
+      <p className="subscribe__copy">
+        Subscribe to keep up with fresh news and exciting updates. We promise not to spam you!
+      </p>
+      <div className="form">
+        <input
+          type="email"
+          className="form__email"
+          placeholder="Enter your email address"
+        />
+        <button className="form__button">Send</button>
+      </div>
+      <div className="notice">
+        <span className="notice__copy">
+        <input type="checkbox" />
+          I agree to my email address being stored and used to receive the monthly newsletter.
+        </span>
+      </div>
+    </div>
     </div>
   );
 };
