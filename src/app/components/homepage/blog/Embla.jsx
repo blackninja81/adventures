4237e99e86aae391a6418ac1d268f6cbce7746ae
John Kaburu <32298219+blackninja81@users.noreply.github.com>
2024-12-17 09:24:43 +0300

    [FEATURE] made it mobile responsive

---
+++ src/app/components/homepage/blog/Embla.jsx
@@ -0,0 +1,47 @@
+import React from "react";
+import { DotButton, useDotButton } from "./EmblaCarouselDotButton";
+import { PrevButton, NextButton, usePrevNextButtons } from "./EmblaCarouselArrowButtons";
+import useEmblaCarousel from "embla-carousel-react";
+
+const EmblaCarousel = ({ slides, options }) => {
+  const [emblaRef, emblaApi] = useEmblaCarousel(options);
+
+  const { selectedIndex, scrollSnaps, onDotButtonClick } = useDotButton(emblaApi);
+  const { prevBtnDisabled, nextBtnDisabled, onPrevButtonClick, onNextButtonClick } = usePrevNextButtons(emblaApi);
+
+  return (
+    <section className="embla">
+      <div className="embla__viewport" ref={emblaRef}>
+        <div className="embla__container">
+          {slides.map((slide, index) => (
+            <div className="embla__slide" key={index}>
+              <img src={slide.image} alt={slide.title} className="embla__image" />
+              <h3>{slide.title}</h3>
+              <h4>{slide.description}</h4>
+              <button>Read More...</button>
+            </div>
+          ))}
+        </div>
+      </div>
+
+      <div className="embla__controls">
+        <div className="embla__buttons">
+          <PrevButton onClick={onPrevButtonClick} disabled={prevBtnDisabled} />
+          <NextButton onClick={onNextButtonClick} disabled={nextBtnDisabled} />
+        </div>
+
+        <div className="embla__dots">
+          {scrollSnaps.map((_, index) => (
+            <DotButton
+              key={index}
+              onClick={() => onDotButtonClick(index)}
+              className={'embla__dot' + (index === selectedIndex ? ' embla__dot--selected' : '')}
+            />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default EmblaCarousel;
